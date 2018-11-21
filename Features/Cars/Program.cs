6c78ed35860e0f5c42d09e34d3dff19943410407
Kevin Villar <kevin.villar@intelectix.com>
2018-11-20 17:46:48 -0700

    Initial commit

---
+++ Features/Cars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name);
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return File.ReadAllLines(path)
+               .Skip(1)
+               .Where(line => line.Length > 1)
+               .Select(Car.ParseFromCsv).ToList();
+        }
+    }
+}
